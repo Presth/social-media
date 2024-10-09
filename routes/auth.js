2aea34bf2f04c61e71edcf2c8c62c0e9fd7f23c1
Precious <88817214+Presth@users.noreply.github.com>
2024-10-09 13:05:17 +0100

    Delete routes directory

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { login, logout, createAccount } from "../controllers/auth.js";
-
-const router = Router()
-router.post("/login", login);
-router.post("/logout", logout);
-router.post("/create-account", createAccount);
-
-export default router;
